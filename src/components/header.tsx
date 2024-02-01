88317d375646c9a75af8e23d86ac75799819306a
brunolepis <brunka215@gmail.com>
2024-02-01 15:41:43 +0100

    refactor: use app description constant in main page header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,12 @@
+import { app } from "@/lib/constants";
+
 import { Icon } from "./icon";
 
 export function Header() {
   return (
     <div className="center py-auto flex h-56 w-full flex-col gap-4 md:h-72">
       <Icon />
-      <p className="max-w-80 text-center">
-        Find songs that are similar to your favorite track on Spotify
-        effortlessly.
-      </p>
+      <p className="max-w-80 text-center">{app.description}</p>
     </div>
   );
 }
